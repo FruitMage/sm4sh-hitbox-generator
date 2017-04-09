a05bad462d8b8ae96a76b3ac60b111c14667c96b
FruitMage <fruitymage@gmail.com>
2017-04-08 21:40:10 -0500

    Fixed Effect setting

--- Sm4sh Hitbox Generator/Form1.cs
+++ Sm4sh Hitbox Generator/Form1.cs
@@ -72,124 +72,124 @@ namespace Sm4sh_Hitbox_Generator
 
         public void setEffect()
         {
-            if (effect.SelectedText == "Normal")
+            if (effect.Text == "Normal")
             {
                 _effect = 0;
             }
-            if (effect.SelectedText == "Slash")
+            if (effect.Text == "Slash")
             {
                 _effect = 2;
             }
-            if (effect.SelectedText == "Electric")
+            if (effect.Text == "Electric")
             {
                 _effect = 3;
             }
-            if (effect.SelectedText == "Freezing")
+            if (effect.Text == "Freezing")
             {
                 _effect = 4;
             }
-            if (effect.SelectedText == "Flame")
+            if (effect.Text == "Flame")
             {
                 _effect = 5;
             }
-            if (effect.SelectedText == "Coin")
+            if (effect.Text == "Coin")
             {
                 _effect = 6;
             }
-            if (effect.SelectedText == "Reverse")
+            if (effect.Text == "Reverse")
             {
                 _effect = 7;
             }
-            if (effect.SelectedText == "Slip")
+            if (effect.Text == "Slip")
             {
                 _effect = 8;
             }
-            if (effect.SelectedText == "Sleep")
+            if (effect.Text == "Sleep")
             {
                 _effect = 9;
             }
-            if (effect.SelectedText == "Bury")
+            if (effect.Text == "Bury")
             {
                 _effect = 11;
             }
-            if (effect.SelectedText == "Stun")
+            if (effect.Text == "Stun")
             {
                 _effect = 12;
             }
-            if (effect.SelectedText == "Flower")
+            if (effect.Text == "Flower")
             {
                 _effect = 14;
             }
-            if (effect.SelectedText == "Death (Insta-Kill)")
+            if (effect.Text == "Death (Insta-Kill)")
             {
                 _effect = 16;
             }
-            if (effect.SelectedText == "Grass (Unused)")
+            if (effect.Text == "Grass (Unused)")
             {
                 _effect = 17;
             }
-            if (effect.SelectedText == "Water")
+            if (effect.Text == "Water")
             {
                 _effect = 18;
             }
-            if (effect.SelectedText == "Darkness")
+            if (effect.Text == "Darkness")
             {
                 _effect = 19;
             }
-            if (effect.SelectedText == "Paralyze")
+            if (effect.Text == "Paralyze")
             {
                 _effect = 20;
             }
-            if (effect.SelectedText == "Aura")
+            if (effect.Text == "Aura")
             {
                 _effect = 21;
             }
-            if (effect.SelectedText == "Plunge")
+            if (effect.Text == "Plunge")
             {
                 _effect = 22;
             }
-            if (effect.SelectedText == "Down (Unused)")
+            if (effect.Text == "Down (Unused)")
             {
                 _effect = 23;
             }
-            if (effect.SelectedText == "Adhension")
+            if (effect.Text == "Adhension")
             {
                 _effect = 24;
                 MessageBox.Show("WARNING: The Adhension effect will crash the game if it is set to a normal move!", "Sm4sh Hitbox Generator");
             }
-            if (effect.SelectedText == "Stab")
+            if (effect.Text == "Stab")
             {
                 _effect = 25;
             }
-            if (effect.SelectedText == "Magic")
+            if (effect.Text == "Magic")
             {
                 _effect = 26;
             }
-            if (effect.SelectedText == "Flinchless #1")
+            if (effect.Text == "Flinchless #1")
             {
                 _effect = 27;
             }
-            if (effect.SelectedText == "Flinchless #2")
+            if (effect.Text == "Flinchless #2")
             {
                 _effect = 28;
             }
-            if (effect.SelectedText == "Solar")
+            if (effect.Text == "Solar")
             {
                 _effect = 29;
             }
-            if (effect.SelectedText == "Focus Attack")
+            if (effect.Text == "Focus Attack")
             {
                 _effect = 30;
             }
-            if (effect.SelectedText == "Disable")
+            if (effect.Text == "Disable")
             {
                 _effect = 31;
             }
-            if (effect.SelectedText == "Dragon Lunge")
+            if (effect.Text == "Dragon Lunge")
             {
                 _effect = 32;
             }
-            if (effect.SelectedText == "Bullet Climax")
+            if (effect.Text == "Bullet Climax")
             {
                 _effect = 36;
             }
